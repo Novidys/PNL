5dc862dfc7734f4d57d6e3599c231db791fed2fd
CyrilleFranchet <cf.poubelle@gmail.com>
2019-05-24 16:13:51 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+VERSION = '0.1'
+
+setup(
+    name='PNL',
+    version=VERSION,
+    packages=['PNL'],
+    download_url='https://github.com/Novidys/pnl',
+    license='',
+    author='CyrilleFranchet',
+    author_email='cyrille.franchet@novidys.com',
+    description='Script to parse new leak files',
+    scripts=['bin/pnl'],
+    package_data={
+        'pnl': ['VERSION'],
+    })
